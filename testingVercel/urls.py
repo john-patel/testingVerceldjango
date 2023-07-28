d483e41fb3f4c94a3351042b3d92c26200d4ccfa
Parth Patel <58204750+john-patel@users.noreply.github.com>
2023-07-28 14:16:07 +0530

    new update from parthcxx

--- testingVercel/urls.py
+++ testingVercel/urls.py
@@ -1,3 +1,5 @@
+<<<<<<< HEAD
+=======
 """
 URL configuration for testingVercel project.
 
@@ -5,7 +7,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
+    1. Add an import:  from my_app imrt views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
@@ -15,23 +17,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import settings
+>>>>>>> fc05f82c7e8cbc49cac12c9a1033705f96295495
 from django.contrib import admin
 from django.urls import path
 from APP import views
 from django.conf.urls.static import static
+from django.conf import settings
+
 
 print("Hellow World")
 
 urlpatterns = [
-  path('admin/', admin.site.urls),
-  path('',views.home)
+    path('admin/', admin.site.urls),
+    path('', views.home),
 ]
 
-# your imports
-urlpatterns = [
-    #url paths
-]
-#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# urlpatterns += path('admin/', admin.site.urls)
-# urlpatterns += path('',views.home)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
