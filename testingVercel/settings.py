350a90f5566414e2a1589c7c1c3956f3aa7f5140
Parth Patel <58204750+john-patel@users.noreply.github.com>
2023-07-28 11:49:37 +0530

    Update settings.py

--- testingVercel/settings.py
+++ testingVercel/settings.py
@@ -121,11 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 print(BASE_DIR)
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static')
-]
-STATIC_ROOT = BASE_DIR  / 'staticfiles/'
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'templates/static')
+#]
+#STATIC_ROOT = BASE_DIR  / 'staticfiles/'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
